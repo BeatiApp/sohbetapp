8cb620a90df0a6040d5fb683662f523b73bcfe1b
Berat Kurt <56454493+zekkontro@users.noreply.github.com>
2021-04-26 21:57:39 +0300

    Update apis.dart

--- lib/utilities/apis.dart
+++ lib/utilities/apis.dart
@@ -1,10 +1,1 @@
-const String googleMapsAPI = "AIzaSyAk1Dtou27-CDA2OF5qI9fHT8DHZUXKu8w";
-const String mapsWebApi = "AIzaSyB8olCRIJg-48r8lJNgeh3WVUL-Y0OSeUo";
-const String giphyAPI = "Ezj8vQI9Ax6ySblE3CzNLsBir3BMKEGd";
-const String newsAPI = "61d58e776f144981af530fde2216c9dc";
-const String openWeatherAPI = "a71c5f236d7e012061a4fdb38ad930b4";
-const String APP_ID = "0e546a666047487da2df78b07ccb5477";
-const String Token =
-    "0060e546a666047487da2df78b07ccb5477IADNRqthMZB698/ipdD67O2ojD1iSiw/OZfNRcwtLUi9GTJS5wYAAAAAEAAeXT+czkJoYAEAAQDMQmhg";
-const String fcmToken =
-    "AAAASHpGn1M:APA91bGe9qY7nzVvusRcQq147QefjT1kJ4hV9GOeymEHtk1cl0qv9GpEMVwao_0040Ov4714ixwG8cjHR9IASZS2ZDwCOm-IKj2egXjjXoHmmqEM2W1xvr1FIQqfMm4tFx9mjeZd3HOl";
+/// Private Page
